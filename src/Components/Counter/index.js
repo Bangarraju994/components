b746fad821b49570ffe89b79bc6755b3c0f84316
Raju994 <bangarraju994@gmail.com>
2023-11-28 18:45:33 +0530

    update

--- src/Components/Counter/index.js
+++ src/Components/Counter/index.js
@@ -2,18 +2,21 @@ import { Component } from "react";
 
 class Counter extends Component{
     render(){
+    const divStyle = {textAlign: 'center'}
+
     const counterParagraphStyle = { color: 'red', fontSize: '50px', textAlign: 'center', fontWeight: 'Bold' };
     
+    const buttonStyles = {backgroundColor: 'white', color: 'green', height: '25px', width: '90px', margin: '20px', textAlign: 'center' }
 
         return(
-            <div >
+            <div style={divStyle} >
                 <h1> Counter </h1>
                 <div>
                     <p style={counterParagraphStyle} > 0 </p>
                 </div>
-                <div>
-                    <button> Increase </button>
-                    <button> Decrease </button>
+                <div >
+                    <button style={buttonStyles}> Increase </button>
+                    <button style={buttonStyles}> Decrease </button>
                 </div>
             </div>
         )
