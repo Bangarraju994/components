b6ba0a7cf729dded800bc7f32ef08bead63eca14
Raju994 <bangarraju994@gmail.com>
2023-11-29 11:01:09 +0530

    added Class Components and make them increase and decrease by using state property

--- src/Components/Counter/index.js
+++ src/Components/Counter/index.js
@@ -1,22 +1,42 @@
 import { Component } from "react";
 
 class Counter extends Component{
+
+    state = {count: 0}
+
+    onIncrease = () => {
+        this.setState((prevState) => ({count: prevState.count + 1}))
+    }
+
+    onDecrease = () => {
+        this.setState((prevState) => ({count: prevState.count - 1}))
+    }
+
+    onReset = () => {
+        this.setState({count: 0})
+    }
+
     render(){
+    
     const divStyle = {textAlign: 'center'}
 
     const counterParagraphStyle = { color: 'red', fontSize: '50px', textAlign: 'center', fontWeight: 'Bold' };
     
-    const buttonStyles = {backgroundColor: 'white', color: 'green', height: '25px', width: '90px', margin: '20px', textAlign: 'center' }
+    const buttonStyles = {color: 'green', height: '25px', width: '90px', margin: '20px', textAlign: 'center' }
+
+    const {count} = this.state
+
 
         return(
             <div style={divStyle} >
                 <h1> Counter </h1>
                 <div>
-                    <p style={counterParagraphStyle} > 0 </p>
+                <p style={counterParagraphStyle} > {count} </p>
                 </div>
                 <div >
-                    <button style={buttonStyles}> Increase </button>
-                    <button style={buttonStyles}> Decrease </button>
+                    <button style={buttonStyles} onClick={this.onDecrease}> Decrease </button>
+                    <button style={buttonStyles} onClick={this.onReset}> Reset </button>
+                    <button style={buttonStyles} onClick={this.onIncrease}> Increase  </button>
                 </div>
             </div>
         )
